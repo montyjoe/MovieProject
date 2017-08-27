ae38ad5563ed4b2d9a67786b8d75450ef8caf871
jrhodesy4 <jrhodesy4@gmail.com>
2017-08-26 18:30:33 -0700

    Add files via upload
    added different user profile pages, as well as laid the groundwork for adding a friend and removing them

---
+++ SecondUpdate/main/apps/User_app/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import url
+from . import views
+
+urlpatterns = [
+    #GET
+    url(r'^login$', views.login_page),
+    url(r'^register$', views.register_page),
+    url(r'^profile$', views.profile),
+    url(r'^user/(?P<id>\d+)$', views.user_page),
+
+    #POSTS
+    url(r'^register_account$', views.register_account),
+    url(r'^log_user_in$', views.log_user_in),
+    url(r'^createProfile$', views.createProfile),
+    url(r'^logout$', views.logout),
+    url(r'^connect/(?P<operation>.+)/(?P<pk>\d+)/$', views.change_friends)
+
+
+]
